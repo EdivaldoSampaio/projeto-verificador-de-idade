7cf8e926fbe110840dcd7e541dff2c646e32db34
Edivaldo <aldovsampaio@gmail.com>
2023-02-11 22:32:07 -0300

    commit do projeto
    projeto verificador de idade, aprendendo JavaScritp.

---
+++ srcipt.js
@@ -0,0 +1,56 @@
+function verificar() {
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fano = document.getElementById('txtano')
+  var res = document.querySelector('div#res')
+  if (fano.value.length == 0 || Number(fano.value) > ano) {
+    window.alert('[ERROR] Verifique os dados e tente novamente!')
+  } else {
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var gênero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+
+    if (fsex[0].checked) {
+      gênero = 'Homem'
+
+      if (idade >= 0 && idade < 10) {
+        // CRIANÇA
+        img.setAttribute('src', 'criançahomem.png')
+      } else if (idade < 21) {
+        // JOVEM
+        img.setAttribute('src', 'jovemhomem.png')
+      } else if (idade < 50) {
+        // ADULTO
+        img.setAttribute('src', 'adultohomem.png')
+      } else {
+        //IDOSO
+        img.setAttribute('src', 'idosohomem.png')
+      }
+
+    } else if (fsex[1].checked) {
+      gênero = 'Mulher'
+
+      if (idade >= 0 && idade < 10) {
+        // CRIANÇA MULHER
+        img.setAttribute('src', 'criançamulher.png')
+
+      } else if (idade < 21) {
+        // JOVEM MULHER
+        img.setAttribute('src', 'mulherjovem.png')
+
+      } else if (idade < 50) {
+        // ADULTO MULHER
+        img.setAttribute('src', 'adultomulher.png')
+      } else {
+        //IDOSO MULHER
+        img.setAttribute('src', 'idosamulher.png')
+      }
+    }
+
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+    res.appendChild(img)
+  }
+}
